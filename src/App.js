ffda88500c5413bd9efff8781e6706cbea754a98
Rob1 <robin.pautigny@hetic.net>
2022-01-07 10:35:00 +0100

    bee

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import logo from './bee.png';
 import './App.css';
+import { Helmet } from 'react-helmet'
+
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Bee🐝</title>
+        <meta name="Move fast ! " content="coding with bee ❤️ " />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>
+      Bee🐝
+      </p>
       </header>
     </div>
   );
