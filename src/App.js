437ba03c40f209b40cb687ed073ae6c192419893
Rob1 <robin.pautigny@hetic.net>
2022-01-09 17:12:33 +0100

    trots add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logoNav from './img/logoNav.png'
 import threeDR from './img/right3d.png'
 import threeDL from './img/left3d.png'
 import arrow from './img/arrow.png'
+import trots from './img/trots.png'
 import './App.css';
 import { Helmet } from 'react-helmet'
 
@@ -12,26 +13,30 @@ function App() {
     <div className="App">
       <Helmet>
         <title>Bee🐝</title>
-        <meta name="Move fast ! " content="coding with bee ❤️ " />
+        <meta name="Move fast ! " content="coding with bee ❤️ "/>
       </Helmet>
         <div className='headerNav'>
         <img src={logoNav} className="beeNav" />
-        <ul className='menu'>
-          <li>Test</li>
-          <li>Test</li>
-          <li>Test</li>
-        </ul>
+        <nav className='navBar'>
+          <ul className='menuHead'>
+            <a href='#'><li>PRODUITS</li></a>
+            <a href='#'><li>NEWS</li></a>
+            <a href='#'><li>CONTACT</li></a>
+          </ul>
+        </nav>
         </div>
       <header className="App-header">
       <img src={threeDR} className="Dright" />
       <img src={threeDL} className="Dleft" />
+      <img src={trots} className='trots'/>
         <p className='liberty'>
           Hi, it's <font color="#FFF200">Bee</font>
         </p>
       <p className='ride'>Change your ride</p>
       <div className='union'></div>
+      <a href="#">
       <img src={arrow} className='arrow'/>
-      
+      </a>
       </header>
 
     </div>
